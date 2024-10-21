27eb0a1dbf692cd0d343b1577e36e557c1761ac0
Velt1 <max.lnfritsche@gmail.com>
2024-10-21 09:48:34 +0200

    Add tileset and meta files for Village Props and Ground

---
+++ Tilesets/Ground.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Ground" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Tiles/Village/TX Tileset Ground.png" width="512" height="512"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.101765" y="0" width="32.8701" height="7.8359"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="-0.101765" width="32.9719" height="9.66767"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32.463" height="7.02178"/>
+  </objectgroup>
+ </tile>
+</tileset>
